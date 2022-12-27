c37c53d4af16f43720513534214372c14ea6d359
matiushariman <matius.hariman@gmail.com>
2022-12-27 10:50:23 +0700

    [home] update FollowedArtists

--- apps/home/src/app/components/FollowedArtists/FollowedArtists.tsx
+++ apps/home/src/app/components/FollowedArtists/FollowedArtists.tsx
@@ -5,7 +5,7 @@ export const FollowedArtists = () => {
   const { isLoading } = useGetFollowedArtists();
 
   return (
-    <div className="pt-4 pb-4 pl-6 gap-2 grid">
+    <div className="pt-4 pb-4 pl-6 gap-2 grid bg-gradient-to-r from-[#1db954] to-[#191414]">
       <p className="font-bold">Following</p>
       <div className="h-[148px]">
         {isLoading ? <p>Loading your following...</p> : <FollowedArtistsList />}
--- apps/home/src/app/components/FollowedArtists/FollowedArtistsList/FollowedArtistCard/FollowedArtistCard.tsx
+++ apps/home/src/app/components/FollowedArtists/FollowedArtistsList/FollowedArtistCard/FollowedArtistCard.tsx
@@ -6,7 +6,7 @@ export const FollowedArtistCard = ({
   name,
   images,
 }: FollowedArtistCardProps) => (
-  <div className="flex-none p-3 max-w-[232px] transition hover:bg-[rgba(0,0,0,0.4)] cursor-pointer">
+  <div className="flex-none rounded-md p-3 max-w-[232px] transition hover:bg-[rgba(0,0,0,0.4)] cursor-pointer">
     <div className="flex flex-col items-center justify-around gap-2">
       <img
         src={images[0].url}
