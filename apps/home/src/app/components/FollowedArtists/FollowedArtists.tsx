02a8f2f1345f7871814404d2649d8cafe6493d73
matiushariman <matius.hariman@gmail.com>
2022-12-27 10:41:48 +0700

    [home] update FollowedArtists background

--- apps/home/src/app/components/FollowedArtists/FollowedArtists.tsx
+++ apps/home/src/app/components/FollowedArtists/FollowedArtists.tsx
@@ -5,7 +5,7 @@ export const FollowedArtists = () => {
   const { isLoading } = useGetFollowedArtists();
 
   return (
-    <div className="bg-[#121212] pt-4 pb-4 pl-6 gap-2 grid border-b border-[rgba(0,0,0,0.3)]">
+    <div className="pt-4 pb-4 pl-6 gap-2 grid">
       <p className="font-bold">Following</p>
       <div className="h-[148px]">
         {isLoading ? <p>Loading your following...</p> : <FollowedArtistsList />}
